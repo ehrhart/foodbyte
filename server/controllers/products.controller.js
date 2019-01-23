c06751ef72499718660d0c009255720fe79ce35f
Thibault Ehrhart <thibault.ehrhart@gmail.com>
2019-01-23 11:29:45 +0100

    Fix offset in /products

--- server/controllers/products.controller.js
+++ server/controllers/products.controller.js
@@ -8,9 +8,9 @@ module.exports = {
 async function getAll(req, res) {
   const page = req.query.p || 1;
   const limit = 20;
-  const offset = (page -1 ) * limit;
+  const offset = (page - 1 ) * limit;
   try {
-    return res.json(await Product.find().offset(offset).limit(limit).exec());
+    return res.json(await Product.find().skip(offset).limit(limit).exec());
   } catch (e) {
     return res.json(e);
   }
