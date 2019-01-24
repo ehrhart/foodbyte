a7b54c5c4410b771b1286b0921753577f2ef1310
Haroun AMRI <haroun3amri@gmail.com>
2019-01-24 08:14:56 +0100

    Interface de base  (#4)
    * fix fxlayout layout version and upgrade all dependencies
    
    * add welcomming interfaces and compenents

---
+++ src/app/recipe/recipe-routing.module.ts
@@ -0,0 +1,20 @@
+import {NgModule} from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
+
+import {RecipeComponent} from './recipe/recipe.component';
+
+const routes: Routes = [{
+  path: 'recipe',
+  children: [{
+    path: 'recipe',
+    component: RecipeComponent
+  }]
+}];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+
+export class RecipeRoutingModule {
+}
---
+++ src/app/shops/shops.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ShopsComponent } from './shops/shops.component';
+
+@NgModule({
+  declarations: [ShopsComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class ShopsModule { }
